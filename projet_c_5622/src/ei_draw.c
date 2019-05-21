9fb0b3bf82ffd72d99f69fda8c046094fc2723e7
Paul Chalas <Paul-Romain.Chalas@grenoble-inp.org>
2019-05-21 17:40:28 +0200

    push

--- projet_c_5622/src/ei_draw.c
+++ projet_c_5622/src/ei_draw.c
@@ -6,40 +6,46 @@
 
 
 uint32_t		ei_map_rgba		(ei_surface_t surface, const ei_color_t* color){
-	int* ir;
-	int* ig;
-	int* ib;
-	int* ia;
+	int* ir = malloc(sizeof(int));
+	int* ig = malloc(sizeof(int));
+	int* ib = malloc(sizeof(int));
+	int* ia = malloc(sizeof(int));
 	hw_surface_get_channel_indices(surface, ir, ig, ib, ia);
 	uint32_t ret = 0;
 	if (*ia == -1)
 	{
-			ret = (color->red << (3-*ir)*8) + (color->green << (3-*ig)*8) + (color->blue << (3-*ib)*8) + 255;
+			ret = (color->red << (*ir)*8) + (color->green << (*ig)*8) + (color->blue << (*ib)*8) + 0xff000000;
 	}
 	else
 	{
-			ret = (color->red << (3-*ir)*8) + (color->green << (3-*ig)*8) + (color->blue << (3-*ib)*8) + (color->alpha << *ia*8);
+			ret = (color->red << (*ir)*8) + (color->green << (*ig)*8) + (color->blue << (*ib)*8) + (color->alpha << (*ia)*8);
 	}
 	return ret;
 }
 
 
 void ei_draw_polyline (ei_surface_t surface, const ei_linked_point_t*	first_point, const ei_color_t color, const ei_rect_t* clipper){
+	//allow to write on the pixels
 	hw_surface_lock(surface);
-	// while(first_point != NULL){
-	// 	printf("BOB");
-	// 	first_point = first_point->next;
-	// }
+
+	while(first_point->next != NULL){
+		printf("%i\n", first_point->point.x);
+		first_point = first_point->next;
+	}
+	first_point = first_point->next;
 
 	uint32_t* pixel;
 	pixel = (uint32_t*)hw_surface_get_buffer(surface);
 
-	pixel += 800 * 100 + 50;
-	*pixel = ei_map_rgba(surface, &color);
+	pixel += first_point->point.y * 800 + first_point->point.x;
 
-	//for (int i = 0; i < (800 * 600); i++)
+	for (int i = 0; i < 50; i++){
+		*pixel++ = ei_map_rgba(surface, &color);
+	}
 
 
+
+	//
 	hw_surface_unlock(surface);
 	hw_surface_update_rects(surface, NULL);
 }
