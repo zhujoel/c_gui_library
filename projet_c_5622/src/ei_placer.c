ea412b243cbff59e04a09871b2881709087a4d1d
Paul Chalas <Paul-Romain.Chalas@grenoble-inp.org>
2019-05-28 14:53:57 +0200

    Gestion des widgets 1.0

---
+++ projet_c_5622/src/ei_placer.c
@@ -0,0 +1,18 @@
+#include "../include/ei_placer.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+void ei_place (struct ei_widget_t* widget, ei_anchor_t* anchor, int* x, int* y, int* width, int* height, float* rel_x, float* el_y, float* rel_width, float* rel_height){
+
+}
+
+void ei_placer_run(struct ei_widget_t* widget){
+    
+}
+
+void ei_placer_forget(struct ei_widget_t* widget){
+
+}
